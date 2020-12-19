48927e204d3ec1a8cc2acb5effd087af1a835c28
EmptinessBoy <i@my.huxiaofan.com>
2020-12-19 19:50:11 +0800

    fix bugs

--- src/CommentsMAP.java
+++ src/CommentsMAP.java
@@ -1,7 +1,9 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.wltea.analyzer.IKSegmentation;
 import org.wltea.analyzer.Lexeme;
 
@@ -9,24 +11,26 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+// 参考代码 https://blog.csdn.net/laozhaokun/article/details/38614765
+
 public class CommentsMAP extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     private Text word = new Text();
     private final static IntWritable one = new IntWritable(1);
 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+//        获取文件名字
+//        InputSplit inputSplit = (InputSplit) context.getInputSplit();
+//        String filename = ((FileSplit) inputSplit).getPath().getName();
+//        System.out.println("当前读取的文件名为：" + filename);
+
         String line = value.toString().trim();
-        String[] str = line.split("\t");
-        for (int i = 1; i < str.length; i += 2) {
-            String tmp = str[i];
-            if (tmp.startsWith("http"))
-                continue;
-            List<String> list = segment(tmp);
-            for (String s : list) {
-                word.set(s);
-                context.write(word, one);
-            }
+        List<String> list = segment(line);
+        for (String s : list) {
+            word.set(s);
+            context.write(word, one);
         }
+
     }
 
     private List<String> segment(String str) throws IOException {
--- src/CommentsReduce.java
+++ src/CommentsReduce.java
@@ -3,17 +3,28 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class CommentsReduce extends Reducer<Text, IntWritable,Text,IntWritable> {
+public class CommentsReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
     private IntWritable result = new IntWritable();
-    public void reduce(Text key,Iterable<IntWritable> values,Context context) throws IOException, InterruptedException{
-        int sum = 0;
-        for(IntWritable val : values)
-            sum += val.get();
-        // ci pin da yv 1 cai shu chu
-        if (sum>1){
-            result.set(sum);
-            context.write(key, result);
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        // 中文字
+        Pattern p = Pattern.compile("[\u4e00-\u9fa5]");
+        Matcher m = p.matcher(String.valueOf(key));
+        if (m.find()) {
+            int sum = 0;
+            for (IntWritable val : values)
+                sum += val.get();
+            // ci pin da yv 1 cai shu chu
+            if (sum > 1) {
+                result.set(sum);
+                context.write(key, result);
+            }
+        } else {
+            return;
         }
     }
 }
